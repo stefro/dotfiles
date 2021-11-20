8fb6ba57c751cb7dcf573d95a5c5e9638c3ba730
Stef Rouschop <stef.rouschop@coderunway.com>
2021-11-20 12:22:57 +0100

    wip

--- path.zsh
+++ path.zsh
@@ -1,21 +1,31 @@
-# Load dotfiles binaries
-export PATH="$DOTFILES/bin:$PATH"
-
-# Load Composer tools
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# Make sure PHP 7 and XDEBUG is loaded
+# export PATH="$(brew --prefix homebrew/php/php70)/bin:$PATH"
+# export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_port=9001 remote_host=127.0.0.1 remote_connect_back=0"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
-# We use Node 14 for now because of some compatibility issues
-export PATH="/usr/local/opt/node@14/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/node@14/lib"
-export CPPFLAGS="-I/usr/local/opt/node@14/include"
+# Load custom commands
+export PATH="$DOTFILES/bin:$PATH"
+
+# Local bin directories before anything else
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH=${PATH}:${HOME}/.composer/vendor/bin;
+
+# spark
+export PATH="$HOME/projects/spark-installer:$PATH"
+
+# npm-global
+export PATH="$HOME/.npm-global/bin:$PATH"
+
+# homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
